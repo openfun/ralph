06d98c7779a2389189e384675bce5ee5eec544b2
Julien Maupetit <julien@maupetit.net>
2020-07-06 17:08:50 +0200

    fixup! ✅(fixtures) add server event

--- src/ralph/schemas/edx/server.py
+++ src/ralph/schemas/edx/server.py
@@ -14,6 +14,9 @@ class ServerEventSchema(BaseEventSchema):
     `/event`, `login`, `heartbeat`, `/segmentio/event`, `/performance`
     """
 
+    event_type = fields.Url(required=True, relative=True)
+    event = fields.Str(required=True)
+
     # pylint: disable=no-self-use
     @validates_schema
     def validate_event_type(self, data, **kwargs):
@@ -21,28 +24,26 @@ class ServerEventSchema(BaseEventSchema):
         if data["event_type"] != data["context"]["path"]:
             raise ValidationError("event_type should be equal to context.path")
 
-    # pylint: disable=no-self-argument, no-self-use
-    def validate_event(value):
-        """check that the event field contains a parsable json string
-        with 2 keys `POST` and `GET` and dictionaries as values.
-        As the event field is trunkated at 500 characters, it might be
-        common that it would not be parsable.
+    # pylint: disable=no-self-use
+    @validates_schema
+    def validate_event(self, value):
+        """check that the event field contains a parsable json string with 2
+        keys `POST` and `GET` and dictionaries as values. As the event field
+        is truncated at 500 characters, it might be common that it would not be
+        parsable.
         """
         try:
             event = json.loads(value)
         except json.JSONDecodeError:
-            raise ValidationError("event field should contain a parsable json string")
-        keys = list(event.keys())
-        if len(keys) != 2:
-            raise ValidationError("event field parsed json should have 2 keys")
-        if "POST" not in keys or "GET" not in keys:
-            raise ValidationError(
-                "event field parsed json should contain GET and POST keys"
-            )
+            raise ValidationError("Server event should contain a JSON string")
+
+        if len(event) != 2:
+            raise ValidationError("Server event field should exactly have two keys")
+
+        if "POST" not in event or "GET" not in event:
+            raise ValidationError("Server event should contain GET and POST keys")
+
         if not isinstance(event["POST"], dict) or not isinstance(event["GET"], dict):
             raise ValidationError(
-                "event.get and event.post values should be dictionaries"
+                "Server event GET and POST values should be serialized objects"
             )
-
-    event_type = fields.Url(required=True, relative=True)
-    event = fields.Str(required=True, validate=validate_event)
