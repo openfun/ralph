a5c8eea3222e7c7a49553b45966f85f58624196e
SergioSim <Sergio.Simonian@etu.unice.fr>
2020-10-08 09:49:20 +0300

    openassessment.student_training_assess_example Schema

---
+++ tests/schema/edx/ora/test_student_training_assess_example.py
@@ -0,0 +1,10 @@
+"""
+Tests for the StudentTrainingAssessExample event schema
+"""
+from ralph.schemas.edx.ora.student_training_assess_example import StudentTrainingAssessExampleSchema
+
+from tests.schema.edx.test_common import check_loading_valid_events
+
+def test_loading_valid_events_should_not_raise_exceptions():
+    """check that loading valid events does not raise exceptions"""
+    check_loading_valid_events(StudentTrainingAssessExampleSchema(), "student_training_assess_example")
