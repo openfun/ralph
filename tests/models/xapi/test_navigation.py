145bd59ae070dbd258484aab51a6fdf96107ccd2
Quitterie Lucas <quitterielucas@outlook.fr>
2021-07-05 09:48:33 +0200

    🐛(selector) add selector for navigation events models
    Selector was missing for the navigation events models.

--- tests/models/xapi/test_navigation.py
+++ tests/models/xapi/test_navigation.py
@@ -1,8 +1,11 @@
 """Tests for the xAPI navigation statements"""
 
+import json
+
 from hypothesis import given, provisional, settings
 from hypothesis import strategies as st
 
+from ralph.models.selector import ModelSelector
 from ralph.models.xapi.fields.actors import ActorAccountField, ActorField
 from ralph.models.xapi.navigation.fields.objects import PageObjectField
 from ralph.models.xapi.navigation.statements import PageTerminated, PageViewed
@@ -31,6 +34,29 @@ def test_models_xapi_page_terminated_statement(statement):
 @settings(max_examples=1)
 @given(
     st.builds(
+        PageTerminated,
+        actor=st.builds(
+            ActorField,
+            account=st.builds(
+                ActorAccountField, name=st.just("username"), homePage=provisional.urls()
+            ),
+        ),
+        object=st.builds(PageObjectField, id=provisional.urls()),
+    )
+)
+def test_models_xapi_page_terminated_selector_with_valid_statement(statement):
+    """Tests given a page terminated statement, the get_model method should return PageTerminated
+    model."""
+
+    statement = json.loads(statement.json())
+    assert (
+        ModelSelector(module="ralph.models.xapi").get_model(statement) is PageTerminated
+    )
+
+
+@settings(max_examples=1)
+@given(
+    st.builds(
         PageViewed,
         actor=st.builds(
             ActorField,
@@ -46,3 +72,23 @@ def test_models_xapi_page_viewed_statement(statement):
 
     assert statement.verb.id == "http://id.tincanapi.com/verb/viewed"
     assert statement.object.definition.type == "http://activitystrea.ms/schema/1.0/page"
+
+
+@settings(max_examples=1)
+@given(
+    st.builds(
+        PageViewed,
+        actor=st.builds(
+            ActorField,
+            account=st.builds(
+                ActorAccountField, name=st.just("username"), homePage=provisional.urls()
+            ),
+        ),
+        object=st.builds(PageObjectField, id=provisional.urls()),
+    )
+)
+def test_models_xapi_page_viewed_selector_with_valid_statement(statement):
+    """Tests given a page viewed statement, the get_model method should return PageViewed model."""
+
+    statement = json.loads(statement.json())
+    assert ModelSelector(module="ralph.models.xapi").get_model(statement) is PageViewed
