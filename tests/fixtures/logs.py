e255b79203f6d07efda532f40fefe3468b0d3936
Quitterie Lucas <quitterielucas@outlook.fr>
2021-07-07 14:42:55 +0200

    🐛(fixture) fix context management for gelf logger fixture
    With the upgrade of pylint package, the stream handler requires to use
    a context. It was ignored in the pull request concerning the upgrade,
    and fix here, additionnally with yielding the logger.

--- tests/fixtures/logs.py
+++ tests/fixtures/logs.py
@@ -12,15 +12,15 @@ from logging_gelf.formatters import GELFFormatter
 def gelf_logger():
     """Generate a GELF logger to generate wrapped tracking log fixtures."""
 
-    # pylint: disable=consider-using-with
-    handler = logging.StreamHandler(NamedTemporaryFile(mode="w+", delete=False))
-    handler.setLevel(logging.INFO)
-    handler.setFormatter(GELFFormatter(null_character=False))
+    with NamedTemporaryFile(mode="w+", delete=False) as temp_file:
+        handler = logging.StreamHandler(temp_file)
+        handler.setLevel(logging.INFO)
+        handler.setFormatter(GELFFormatter(null_character=False))
 
-    # Generate a unique logger per test function to avoid stacking handlers on
-    # the same one
-    logger = logging.getLogger(f"test_logger_{token_hex(8)}")
-    logger.setLevel(logging.INFO)
-    logger.addHandler(handler)
+        # Generate a unique logger per test function to avoid stacking handlers on
+        # the same one
+        logger = logging.getLogger(f"test_logger_{token_hex(8)}")
+        logger.setLevel(logging.INFO)
+        logger.addHandler(handler)
 
-    return logger
+        yield logger
