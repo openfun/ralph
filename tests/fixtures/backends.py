1e079249194f5a1c83269147f984ae8a638f6c29
Julien Maupetit <julien@maupetit.net>
2021-07-15 12:26:12 +0200

    🐛(backends:es) fix bulk requests datastream compatibility
    Performing bulk requests with a fixed type fails while using a
    datastream (instead of a standard index) due to mapping inconsistency
    errors.

--- tests/fixtures/backends.py
+++ tests/fixtures/backends.py
@@ -9,7 +9,9 @@ from elasticsearch import Elasticsearch
 from ralph.backends.storage.swift import SwiftStorage
 
 # Elasticsearch backend defaults
-ES_TEST_INDEX = os.environ.get("RALPH_ES_TEST_INDEX", "test-index")
+ES_TEST_INDEX = os.environ.get("RALPH_ES_TEST_INDEX", "test-index-foo")
+ES_TEST_INDEX_TEMPLATE = os.environ.get("RALPH_ES_TEST_INDEX_TEMPLATE", "test-index")
+ES_TEST_INDEX_PATTERN = os.environ.get("RALPH_ES_TEST_INDEX_PATTERN", "test-index-*")
 ES_TEST_HOSTS = os.environ.get("RALPH_ES_TEST_HOSTS", "http://localhost:9200").split(
     ","
 )
@@ -43,6 +45,50 @@ def es():
     client.indices.create(index=ES_TEST_INDEX)
     yield client
     client.indices.delete(index=ES_TEST_INDEX)
+
+
+@pytest.fixture
+def es_data_stream():
+    """Creates / deletes an ElasticSearch test datastream and yields an instantiated client."""
+
+    client = Elasticsearch(ES_TEST_HOSTS)
+
+    # Create statements index template with enabled data stream
+    index_template = {
+        "index_patterns": [ES_TEST_INDEX_PATTERN],
+        "data_stream": {},
+        "template": {
+            "mappings": {
+                "dynamic": True,
+                "dynamic_date_formats": [
+                    "strict_date_optional_time",
+                    "yyyy/MM/dd HH:mm:ss Z||yyyy/MM/dd Z",
+                ],
+                "dynamic_templates": [],
+                "date_detection": True,
+                "numeric_detection": True,
+            },
+            "settings": {
+                "index": {
+                    "number_of_shards": "1",
+                    "number_of_replicas": "1",
+                }
+            },
+        },
+    }
+    client.transport.perform_request(
+        "PUT", f"/_index_template/{ES_TEST_INDEX_TEMPLATE}", body=index_template
+    )
+
+    # Create a datastream matching the index template
+    client.indices.create_data_stream(ES_TEST_INDEX)
+
+    yield client
+
+    client.indices.delete_data_stream(ES_TEST_INDEX)
+    client.transport.perform_request(
+        "DELETE", f"/_index_template/{ES_TEST_INDEX_TEMPLATE}"
+    )
 
 
 @pytest.fixture
