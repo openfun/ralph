587578b2528eac0b65e83ffc5043e4e63fffe908
Quitterie Lucas <quitterielucas@outlook.fr>
2022-10-31 15:55:14 +0100

    ✨(models) add edx open response assessment pydantic event models
    ORA2 events describe the activity for assessments pair reviewing.
    Edx defined models and their selectors are described with Pydantic.

---
+++ tests/models/edx/open_response_assessment/test_events.py
@@ -0,0 +1,87 @@
+"""Tests for open response assessment statement event fields."""
+
+import json
+import re
+
+import pytest
+from pydantic import ValidationError
+
+from ralph.models.edx.open_response_assessment.fields.events import (
+    ORAAssessEventField,
+    ORAAssessEventRubricField,
+    ORAGetPeerSubmissionEventField,
+    ORAGetSubmissionForStaffGradingEventField,
+)
+
+from tests.fixtures.hypothesis_strategies import custom_given
+
+
+@custom_given(ORAGetPeerSubmissionEventField)
+def test_models_edx_ora_get_peer_submission_event_field_with_valid_values(field):
+    """Tests that a valid `ORAGetPeerSubmissionEventField` does not raise a
+    `ValidationError`."""
+
+    assert re.match(
+        r"^block-v1:.+\+.+\+.+type@openassessment+block@[a-f0-9]{32}$", field.item_id
+    )
+
+
+@custom_given(ORAGetSubmissionForStaffGradingEventField)
+def test_models_edx_ora_get_submission_for_staff_grading_event_field_with_valid_values(
+    field,
+):
+    """Tests that a valid `ORAGetSubmissionForStaffGradingEventField` does not raise a
+    `ValidationError`."""
+
+    assert re.match(
+        r"^block-v1:.+\+.+\+.+type@openassessment+block@[a-f0-9]{32}$", field.item_id
+    )
+
+
+@custom_given(ORAAssessEventField)
+def test_models_edx_ora_assess_event_field_with_valid_values(field):
+    """Tests that a valid `ORAAssessEventField` does not raise a
+    `ValidationError`.
+    """
+
+    assert field.score_type in {"PE", "SE", "ST"}
+
+
+@pytest.mark.parametrize(
+    "score_type",
+    ["pe", "se", "st", "SA", "PA", "22", "&T"],
+)
+@custom_given(ORAAssessEventField)
+def test_models_edx_ora_assess_event_field_with_invalid_values(score_type, field):
+    """Tests that invalid `ORAAssessEventField` raises a `ValidationError`."""
+
+    invalid_field = json.loads(field.json())
+    invalid_field["score_type"] = score_type
+
+    with pytest.raises(ValidationError, match="score_type\n  unexpected value"):
+        ORAAssessEventField(**invalid_field)
+
+
+@pytest.mark.parametrize(
+    "content_hash",
+    [
+        "d0d4a647742943e3951b45d9db8a0ea1ff71ae3&",
+        "d0d4a647742943e3951b45d9db8a0ea1ff71ae360",
+        "D0d4a647742943e3951b45d9db8a0ea1ff71ae36",
+    ],
+)
+@custom_given(ORAAssessEventRubricField)
+def test_models_edx_ora_assess_event_rubric_field_with_invalid_problem_id_value(
+    content_hash, field
+):
+    """Tests that an invalid `problem_id` value in `ProblemCheckEventField` raises a
+    `ValidationError`.
+    """
+
+    invalid_field = json.loads(field.json())
+    invalid_field["content_hash"] = content_hash
+
+    with pytest.raises(
+        ValidationError, match="content_hash\n  string does not match regex"
+    ):
+        ORAAssessEventRubricField(**invalid_field)
---
+++ tests/models/edx/open_response_assessment/test_statements.py
@@ -0,0 +1,152 @@
+"""Tests for the open response assessment statement models."""
+
+import json
+
+import pytest
+from hypothesis import strategies as st
+
+from ralph.models.edx.open_response_assessment.statements import (
+    ORACreateSubmission,
+    ORAGetPeerSubmission,
+    ORAGetSubmissionForStaffGrading,
+    ORAPeerAssess,
+    ORASaveSubmission,
+    ORASelfAssess,
+    ORAStaffAssess,
+    ORAStudentTrainingAssessExample,
+    ORASubmitFeedbackOnAssessments,
+    ORAUploadFile,
+)
+from ralph.models.selector import ModelSelector
+
+from tests.fixtures.hypothesis_strategies import custom_builds, custom_given
+
+
+@pytest.mark.parametrize(
+    "class_",
+    [
+        ORASaveSubmission,
+        ORACreateSubmission,
+        ORAStaffAssess,
+        ORAPeerAssess,
+        ORASelfAssess,
+        ORAGetPeerSubmission,
+        ORAGetSubmissionForStaffGrading,
+        ORAUploadFile,
+        ORASubmitFeedbackOnAssessments,
+        ORAStudentTrainingAssessExample,
+    ],
+)
+@custom_given(st.data())
+def test_models_edx_ora_selectors_with_valid_statements(class_, data):
+    """Tests given a valid open response assessment edX statement the `get_first_model`
+    selector method should return the expected model.
+    """
+
+    statement = json.loads(data.draw(custom_builds(class_)).json())
+    model = ModelSelector(module="ralph.models.edx").get_first_model(statement)
+    assert model is class_
+
+
+@custom_given(ORAGetPeerSubmission)
+def test_models_edx_ora_get_peer_submission_with_valid_statement(statement):
+    """Tests that a `openassessmentblock.get_peer_submission` statement has the expected
+    `event_type` and `page` fields.
+    """
+
+    assert statement.event_type == "openassessmentblock.get_peer_submission"
+    assert statement.page == "x_module"
+
+
+@custom_given(ORAGetSubmissionForStaffGrading)
+def test_models_edx_ora_get_submission_for_staff_grading_with_valid_statement(
+    statement,
+):
+    """Tests that a `openassessmentblock.get_submission_for_staff_grading` statement has
+    the expected `event_type` and `page` fields.
+    """
+
+    assert (
+        statement.event_type == "openassessmentblock.get_submission_for_staff_grading"
+    )
+    assert statement.page == "x_module"
+
+
+@custom_given(ORAPeerAssess)
+def test_models_edx_ora_peer_assess_with_valid_statement(statement):
+    """Tests that a `openassessmentblock.peer_assess` statement has the expected
+    `event_type` and `page` fields.
+    """
+
+    assert statement.event_type == "openassessmentblock.peer_assess"
+    assert statement.page == "x_module"
+
+
+@custom_given(ORASelfAssess)
+def test_models_edx_ora_self_assess_with_valid_statement(statement):
+    """Tests that a `openassessmentblock.self_assess` statement has the expected
+    `event_type` and `page` fields.
+    """
+
+    assert statement.event_type == "openassessmentblock.self_assess"
+    assert statement.page == "x_module"
+
+
+@custom_given(ORAStaffAssess)
+def test_models_edx_ora_staff_assess_with_valid_statement(statement):
+    """Tests that a `openassessmentblock.staff_assess` statement has the expected
+    `event_type` and `page` fields.
+    """
+
+    assert statement.event_type == "openassessmentblock.staff_assess"
+    assert statement.page == "x_module"
+
+
+@custom_given(ORASubmitFeedbackOnAssessments)
+def test_models_edx_ora_submit_feedback_on_assessments_with_valid_statement(statement):
+    """Tests that a `openassessmentblock.submit_feedback_on_assessments` statement has
+    the expected `event_type` and `page` fields.
+    """
+
+    assert statement.event_type == "openassessmentblock.submit_feedback_on_assessments"
+    assert statement.page == "x_module"
+
+
+@custom_given(ORACreateSubmission)
+def test_models_edx_ora_create_submission_with_valid_statement(statement):
+    """Tests that a `openassessmentblock.create_submission` statement has the expected
+    `event_type` and `page` fields.
+    """
+
+    assert statement.event_type == "openassessmentblock.create_submission"
+    assert statement.page == "x_module"
+
+
+@custom_given(ORASaveSubmission)
+def test_models_edx_ora_save_submission_with_valid_statement(statement):
+    """Tests that a `openassessmentblock.save_submission` statement has the expected
+    `event_type` and `page` fields.
+    """
+
+    assert statement.event_type == "openassessmentblock.save_submission"
+    assert statement.page == "x_module"
+
+
+@custom_given(ORAStudentTrainingAssessExample)
+def test_models_edx_ora_student_training_assess_example_with_valid_statement(statement):
+    """Tests that a `openassessment.student_training_assess_example` statement
+    has the expected `event_type` and `page` fields.
+    """
+
+    assert statement.event_type == "openassessment.student_training_assess_example"
+    assert statement.page == "x_module"
+
+
+@custom_given(ORAUploadFile)
+def test_models_edx_ora_upload_file_example_with_valid_statement(statement):
+    """Tests that a `openassessment.upload_file` statement
+    has the expected `event_type` and `page` fields.
+    """
+
+    assert statement.event_type == "openassessment.upload_file"
+    assert statement.name == "openassessment.upload_file"
