e99e8ed82cf259d94a06e7cf10248db7b20647b5
SergioSim <Sergio.Simonian@etu.unice.fr>
2022-11-04 11:34:24 +0100

    ✅(tests) fix hypothesis warnings in tests
    The new version of hypothesis emits a warning during boolean comparison
    of a hypothesis strategy. Thus we choose replace the comparison by
    checking for the `None` value instead.

--- tests/fixtures/hypothesis_strategies.py
+++ tests/fixtures/hypothesis_strategies.py
@@ -65,9 +65,9 @@ def custom_builds(
         arg = kwargs.get(name, None)
         if arg is False:
             continue
-        is_required = field.required or (arg and _overwrite_default)
-        required_optional = required if is_required or arg else optional
-        field_strategy = arg if arg else get_strategy_from(field.outer_type_)
+        is_required = field.required or (arg is not None and _overwrite_default)
+        required_optional = required if is_required or arg is not None else optional
+        field_strategy = get_strategy_from(field.outer_type_) if arg is None else arg
         required_optional[field.alias] = field_strategy
     if not required:
         # To avoid generating empty values
