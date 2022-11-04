dec54b8abda091020393b90fe88ac14a0c434fd4
SergioSim <Sergio.Simonian@etu.unice.fr>
2022-11-04 11:43:53 +0100

    ✅(tests) remove custom JSON strategy resolver
    The new version of pydantic includes our fix for resolving nested JSON
    fields. Thus this eliminates the need for a custom JSON strategy
    resolver.

--- tests/fixtures/hypothesis_configuration.py
+++ tests/fixtures/hypothesis_configuration.py
@@ -4,28 +4,9 @@ import operator
 
 from hypothesis import provisional, settings
 from hypothesis import strategies as st
-from pydantic import AnyHttpUrl, AnyUrl, BaseModel, Json, JsonWrapper, StrictStr
-from pydantic._hypothesis_plugin import RESOLVERS, resolve_json
+from pydantic import AnyHttpUrl, AnyUrl, StrictStr
 
 from ralph.models.xapi.fields.common import IRI, LanguageTag, MailtoEmail
-
-
-def is_base_model(klass):
-    """Returns True if the given class is a subclass of the pydantic BaseModel."""
-
-    try:
-        return issubclass(klass, BaseModel)
-    except TypeError:
-        return False
-
-
-def custom_resolve_json(cls):
-    """Returns a hypothesis build strategy for Json types."""
-
-    if not is_base_model(getattr(cls, "inner_type", None)):
-        return resolve_json(cls)
-    return st.builds(cls.inner_type.json, st.from_type(cls.inner_type))
-
 
 settings.register_profile("development", max_examples=1)
 settings.load_profile("development")
@@ -39,5 +20,3 @@ st.register_type_strategy(
     MailtoEmail, st.builds(operator.add, st.just("mailto:"), st.emails())
 )
 st.register_type_strategy(LanguageTag, st.just("en-US"))
-st.register_type_strategy(Json, custom_resolve_json)
-RESOLVERS[JsonWrapper] = custom_resolve_json
