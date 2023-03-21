8dd10bf53a4c6929a8f4345fa1e9727d5af17fb5
Quitterie Lucas <quitterielucas@outlook.fr>
2023-03-21 12:41:51 +0100

    ✅(backends) add test for base http backend
    Add test for base http backend to ensure good instantiation of the backend base
    class.

---
+++ tests/backends/http/test_base.py
@@ -0,0 +1,47 @@
+"""Tests for Ralph base HTTP backend."""
+
+from typing import Iterator, List, Union
+
+from ralph.backends.http.base import BaseHTTP, BaseQuery, OperationType
+
+
+def test_backends_http_base_abstract_interface_with_implemented_abstract_method():
+    """Tests the interface mechanism with properly implemented abstract methods."""
+
+    class GoodStorage(BaseHTTP):
+        """Correct implementation with required abstract methods."""
+
+        name = "good"
+
+        def status(self):
+            """Fakes the status method."""
+
+        def list(
+            self, target: str = None, details: bool = False, new: bool = False
+        ) -> Iterator[Union[str, dict]]:
+            """Fakes the list method."""
+
+        def read(  # pylint: disable=too-many-arguments
+            self,
+            query: Union[str, BaseQuery] = None,
+            target: str = None,
+            chunk_size: Union[None, int] = None,
+            raw_output: bool = False,
+            ignore_errors: bool = False,
+        ):
+            """Fakes the read method."""
+
+        def write(  # pylint: disable=too-many-arguments
+            self,
+            data: Union[List[bytes], List[dict]],
+            target: Union[None, str] = None,
+            chunk_size: Union[None, int] = 500,
+            ignore_errors: bool = False,
+            operation_type: Union[None, OperationType] = None,
+        ):
+            """Fakes the write method."""
+
+    GoodStorage()
+
+    assert GoodStorage.name == "good"
+    assert GoodStorage.query == BaseQuery
