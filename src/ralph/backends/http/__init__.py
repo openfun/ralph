1c3caa057e634cdc9e0147b4dd22123500e60657
lleeoo <leo@bouloc.eu>
2023-04-04 19:28:05 +0200

    :sparkles:(backends) add http backend type

---
+++ src/ralph/backends/http/__init__.py
@@ -0,0 +1,4 @@
+"""HTTP backends for Ralph."""
+
+from .base import BaseHTTP  # noqa: F401
+from .lrs_async import LRSHTTP  # noqa: F401
---
+++ src/ralph/backends/http/base.py
@@ -0,0 +1,28 @@
+"""Base HTTP backend for Ralph."""
+
+import logging
+from abc import ABC, abstractmethod
+from typing import Iterable, Iterator, Union
+
+logger = logging.getLogger(__name__)
+
+
+class BaseHTTP(ABC):
+    """Base HTTP backend interface."""
+
+    name = "base"
+
+    @abstractmethod
+    def async_get_statements(
+        self, target: str, chunk_size: Union[None, int] = None
+    ) -> Iterator[Union[bytes, dict]]:
+        """Yields records read from the HTTP response results."""
+
+    @abstractmethod
+    def async_post_statements(
+        self,   
+        target: str,
+        data: Union[dict, Iterable[dict]],
+        chunk_size: Union[None, int] = None
+    ) -> int:
+        """POST request to the HTTP server given an input endpoint."""
