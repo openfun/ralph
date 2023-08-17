1bfdb7764494616b59ca310c3f7d2a27afbf70b8
Wilfried BARADAT <wilfried.baradat@fun-mooc.fr>
2023-08-17 09:55:06 +0200

    🏗️(backends) unify stream backend settings
    With the new data backend interface, settings are now close to each backend
    and not under general conf.py.
    Unifying stream backend WS to have the same architecture as data backends.

--- src/ralph/backends/stream/ws.py
+++ src/ralph/backends/stream/ws.py
@@ -6,34 +6,51 @@ from typing import BinaryIO
 
 import websockets
 
-from ralph.conf import settings
+from ralph.conf import BaseSettingsConfig
 
-from .base import BaseStream
+from .base import BaseStreamBackend, BaseStreamBackendSettings
 
 logger = logging.getLogger(__name__)
 
 
-class WSStream(BaseStream):
+class WSStreamBackendSettings(BaseStreamBackendSettings):
+    """Websocket stream backend default configuration.
+
+    Attributes:
+        URI (str): The URI to connect to.
+    """
+
+    class Config(BaseSettingsConfig):
+        """Pydantic Configuration."""
+
+        env_prefix = "RALPH_BACKENDS__STREAM__WS__"
+
+    URI: str = None
+
+
+class WSStreamBackend(BaseStreamBackend):
     """Websocket stream backend."""
 
     name = "ws"
+    settings_class = WSStreamBackendSettings
 
-    def __init__(self, uri: str = settings.BACKENDS.STREAM.WS.URI):
+    def __init__(self, settings: settings_class = None):
         """Instantiate the websocket client.
 
         Args:
-            uri (str): The URI to connect to.
+            settings (WSStreamBackendSettings or None): The stream backend settings.
+                If `settings` is `None`, a default settings instance is used instead.
         """
-        self.uri = uri
+        self.settings = settings if settings else self.settings_class()
 
     def stream(self, target: BinaryIO):
         """Stream websocket content to target."""
         # pylint: disable=no-member
 
-        logger.debug("Streaming from websocket uri: %s", self.uri)
+        logger.debug("Streaming from websocket uri: %s", self.settings.URI)
 
         async def _stream():
-            async with websockets.connect(self.uri) as websocket:
+            async with websockets.connect(self.settings.URI) as websocket:
                 while event := await websocket.recv():
                     target.write(bytes(f"{event}" + "\n", encoding="utf-8"))
 
