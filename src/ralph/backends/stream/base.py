d112e84b4cd308066427d8dfe7ef3a2b7ee6ec2d
Wilfried BARADAT <wilfried.baradat@fun-mooc.fr>
2023-08-17 09:55:06 +0200

    🏗️(backends) unify stream backend settings
    With the new data backend interface, settings are now close to each backend
    and not under general conf.py.
    Unifying stream backend WS to have the same architecture as data backends.

--- src/ralph/backends/stream/base.py
+++ src/ralph/backends/stream/base.py
@@ -3,12 +3,28 @@
 from abc import ABC, abstractmethod
 from typing import BinaryIO
 
+from pydantic import BaseSettings
+
+from ralph.conf import BaseSettingsConfig, core_settings
+
+
+class BaseStreamBackendSettings(BaseSettings):
+    """Data backend default configuration."""
+
+    class Config(BaseSettingsConfig):
+        """Pydantic Configuration."""
+
+        env_prefix = "RALPH_BACKENDS__STREAM__"
+        env_file = ".env"
+        env_file_encoding = core_settings.LOCALE_ENCODING
+
 
 class BaseStream(ABC):
     """Base stream backend interface."""
 
     name = "base"
+    settings_class = BaseStreamBackendSettings
 
     @abstractmethod
     def stream(self, target: BinaryIO):
-        """Read records and streams them to target."""
+        """Read records and stream them to target."""
