635a1ef1751c01cc8e0b1a417e6090c6cfc289e5
piptouque <pierre.thiel@touque.fr>
2026-02-17 15:04:13 +0100

    ✨(oidc) get user claims from /userinfo endpoint
    The ID token and access token are different and
    have different purpose.
    The ID token is always a JWT and contains user claims,
    but the access token may not be.
    With this change, we get the user claims using the access token,
    using the /userinfo OIDC enpoint,
    allowing us to support providers that return opaque access tokens.

--- src/ralph/api/auth/oidc.py
+++ src/ralph/api/auth/oidc.py
@@ -2,7 +2,7 @@
 
 import logging
 from functools import lru_cache
-from typing import Dict, Optional, Union, get_args
+from typing import Dict, Optional, Union, Literal, get_args
 
 import requests
 from fastapi import Depends, HTTPException, status
@@ -26,17 +26,19 @@ oauth2_scheme = OpenIdConnect(
 logger = logging.getLogger(__name__)
 
 
-class IDToken(BaseModel):
-    """Pydantic model representing the core of an OpenID Connect ID Token.
+class UserInfo(BaseModel):
+    """Pydantic model representing the core of an OpenID Connect UserInfo endpoint response.
 
-    ID Tokens are polymorphic and may have many attributes not defined in the
+    They are common to both the ID token and the UserInfo endpoint.
+
+    They are polymorphic and may have many attributes not defined in the
     specification. This model ignores all additional fields.
 
     Attributes:
         iss (str): Issuer Identifier for the Issuer of the response.
         sub (str): Subject Identifier.
-        aud (str or list of str): Audience(s) that this ID Token is intended for.
-        exp (int): Expiration time on or after which the ID Token MUST NOT be
+        aud (str or list of str): Audience(s) that this ID Token/UserInfo is intended for.
+        exp (int): Expiration time on or after which the ID Token/UserInfo MUST NOT be
                    accepted for processing.
         iat (int): Time at which the JWT was issued.
         scope (str): Scope(s) for resource authorization.
@@ -70,6 +72,71 @@ def discover_provider(base_url: AnyUrl) -> Dict:
             detail="Could not validate credentials",
             headers={"WWW-Authenticate": "Bearer"},
         ) from exc
+
+def get_user_info(provider_config: dict, access_token: str) -> UserInfo:
+    """Get the user's info from the IdP using the /userinfo OIDC endpoint."""
+    user_info, is_encoded = get_user_info_data(provider_config["userinfo_endpoint"], access_token)
+    if not is_encoded:
+        # nothing to do
+        return UserInfo.model_validate(user_info)
+    return decode_user_info(user_info, provider_config=provider_config)
+
+
+@lru_cache()
+def get_user_info_data(userinfo_endpoint: AnyUrl, access_token: str) -> Union[tuple[dict, Literal[False]], tuple[str, Literal[True]]]:
+    """Get the user's info from the IdP using the /userinfo OIDC endpoint.
+
+    The data may be unencoded (Content-Type: 'application/json', in which case it is a json dictionary
+    If it is encoded (Content-Type: 'application/jwt', it is a JWT
+    see: https://openid.net/specs/openid-connect-core-1_0.html#UserInfo
+
+    Returns the data and whether that data is encoded (a JWT string) or plain (a dict)
+    """
+    try:
+        response = requests.get(
+            f"{userinfo_endpoint}",
+            headers={"Authorization": f"Bearer {access_token}"},
+            timeout=5,
+        )
+        response.raise_for_status()
+        content_type = response.headers["Content-Type"]
+        return (response.json(), content_type.lower() == "application/jwt")
+    except requests.exceptions.RequestException as exc:
+        logger.error("Unable to get the user's ID token: %s", exc)
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        ) from exc
+
+
+def decode_user_info(encoded_user_info: str, provider_config: dict) -> UserInfo:
+    """Decode and verify an OpenID Connect ID token."""
+    key = get_public_keys(provider_config["jwks_uri"])
+    algorithms = provider_config["id_token_signing_alg_values_supported"]
+    audience = settings.RUNSERVER_AUTH_OIDC_AUDIENCE
+    options = {
+        "verify_signature": True,
+        "verify_aud": bool(audience),
+        "verify_exp": True,
+    }
+    try:
+        decoded_token = jwt.decode(
+            token=encoded_user_info,
+            key=key,
+            algorithms=algorithms,
+            options=options,
+            audience=audience,
+        )
+    except (ExpiredSignatureError, JWTError, JWTClaimsError) as exc:
+        logger.error("Unable to decode the ID token: %s", exc)
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        ) from exc
+
+    return UserInfo.model_validate(decoded_token)
 
 
 @lru_cache()
@@ -130,37 +197,13 @@ def get_oidc_user(
         )
         return None
 
-    id_token = auth_header.split(" ")[-1]
+    access_token = auth_header.split(" ")[-1]
     provider_config = discover_provider(settings.RUNSERVER_AUTH_OIDC_ISSUER_URI)
-    key = get_public_keys(provider_config["jwks_uri"])
-    algorithms = provider_config["id_token_signing_alg_values_supported"]
-    audience = settings.RUNSERVER_AUTH_OIDC_AUDIENCE
-    options = {
-        "verify_signature": True,
-        "verify_aud": bool(audience),
-        "verify_exp": True,
-    }
-    try:
-        decoded_token = jwt.decode(
-            token=id_token,
-            key=key,
-            algorithms=algorithms,
-            options=options,
-            audience=audience,
-        )
-    except (ExpiredSignatureError, JWTError, JWTClaimsError) as exc:
-        logger.error("Unable to decode the ID token: %s", exc)
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Could not validate credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        ) from exc
-
-    id_token = IDToken.model_validate(decoded_token)
-
-    user = AuthenticatedUser(
-        agent={"openid": f"{id_token.iss}/{id_token.sub}"},
-        scopes=get_user_scopes(id_token.scope),
-        target=id_token.target,
+    user_info = get_user_info(
+        provider_config, access_token=access_token
     )
-    return user
+    return AuthenticatedUser(
+        agent={"openid": f"{user_info.iss}/{user_info.sub}"},
+        scopes=get_user_scopes(user_info.scope),
+        target=user_info.target,
+    )
